6e102883467388acbf52e9995762a5cd227ab5cd
Inna Yasynchuk <i_bybyk@ukr.net>
2025-01-21 16:04:28 +0200

    added sections header_ top_ about

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const swiperTop = new Swiper('.top__swiper', {
+ 
+  effect: "fade",
+
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+});
+
+var swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  freeMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
